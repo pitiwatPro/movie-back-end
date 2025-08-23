71228f2ecaddb086caaeb311377d3c289a3e5ffd
pitiwat work <pitiwat.mike.work@gmail.com>
2025-08-23 16:45:41 +0700

    feat: add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/middlewares/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix(process.env.GLOBAL_PREFIX ?? 'api');
+
+  const config = new DocumentBuilder()
+    .setTitle('The Movie API ')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
